137812b0afe93787c213c7bedbe7c386ec0e7293
janluyleonardo <janluy.moreno@gmail.com>
2024-02-21 11:35:56 -0500

    ajuste para que veamos si entra en la vista

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -10,18 +10,18 @@
         <link href="https://fonts.googleapis.com/css2?family=Nunito:wght@400;600;700&display=swap" rel="stylesheet">
 
         <!-- favicon -->
-        <link rel="shortcut icon" href="{{ asset('favicon.ico?v='.now()->format('H.s')) }}" type="image/x-icon">
+        <link rel="shortcut icon" href="{{ secure_asset('favicon.ico?v='.now()->format('H.s')) }}" type="image/x-icon">
 
         <!-- Styles -->
-        <link rel="stylesheet" href="{{ asset('css/bootstrap.css?v='.now()->format('H.s')) }}">
+        <link rel="stylesheet" href="{{ secure_asset('css/bootstrap.css?v='.now()->format('H.s')) }}">
         {{-- <link rel="stylesheet" href="{{ secure_asset('css/welcome.css?v='.now()->format('H.s')) }}"> --}}
-        <link rel="stylesheet" href="{{ asset('css/welcome.css?v='.now()->format('H.s')) }}">
+        <link rel="stylesheet" href="{{ secure_asset('css/welcome.css?v='.now()->format('H.s')) }}">
     </head>
     <body class="antialiased">
         <div class="relative flex items-top justify-center min-h-screen bg-gray-100 dark:bg-gray-900 sm:items-center py-4 sm:pt-0">
             <div class="max-w-6xl mx-auto sm:px-1 lg:px-1">
                 <div class="flex justify-center pt-1 sm:justify-start sm:pt-0">
-                  <img class="sombra" src="{{ asset('images/logo/LOGO.png?v='.now()->format('H.s')) }}" alt="logo-jackeline" width="250">
+                  <img class="sombra" src="{{ secure_asset('images/logo/LOGO.png?v='.now()->format('H.s')) }}" alt="logo-jackeline" width="250">
                 </div>
                 <br>
                 <div class="d-flex justify-content-center">
